dcfbc3b1368e729897b18c9784b8262a1873cb52
ankitbiswas <ankitbiswas008@gmail.com>
2024-12-23 13:23:23 +0530

    source code analysis setup

---
+++ Source_Code_Analysis_GenAI/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ankit Biswas',
+    author_email= 'ankitbiswas008@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
